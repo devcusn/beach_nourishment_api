75222fcffa319845cdceb688ddbc67401962ec0e
Halil Tunc <hitunc@outlook.com>
2024-05-27 18:29:43 +0300

    feat: unnecessary folders removed

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request
 
-from tests.wave_height import WaveHeight
-from tests.wave_height_ml import WaveHeightML
 from services.closure_depth import ClosureDepth
 from services.volume import Volume
 from services.revetment import Revetment
@@ -14,19 +12,6 @@ CORS(app)
 @app.route("/")
 def hello_api():
     return "<p>Hello, welcome to beach nourishment api</p>"
-
-
-@app.route("/api/wave_heigh")
-def calculateWaveHeight():
-    cas = WaveHeight()
-    return str(cas.getWaveLength())
-
-
-@app.route("/api/ml/wave_heigth")
-def calculateWaveHeightML():
-    whML = WaveHeightML()
-    whML.run()
-    return str(whML.getResult())
 
 
 @app.route('/api/closure_depth', methods=['POST'])
